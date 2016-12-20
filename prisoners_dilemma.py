2780f422c5411806433c7394fa9931455ee8d22d
Rocco Zirille <samuel.zirille@students.sacs.k12.in.us>
2016-12-20 10:40:22 -0500

    Rocco Zirille Ex.7
    Ex.7 change

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -33,7 +33,23 @@ from __future__ import print_function
 #######This is Rocco Zirille's #9
 import random
 import os.path              
+team_name = 'E7'
+strategy_name = 'Error'
+strategy_description = 'return \'\', \' \', or int 4'
     
+def move(my_history, their_history, my_score, their_score):
+    '''Make my move based on the history with this player.
+    
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
+    '''
+    
+    #This example player always betrays.      
+    return random.choice(['', ' ', 4]) 
 import example0, example1, example2, example3
 import example4, example5, example6, example7
 import team0, team1, team2, team3, team4
