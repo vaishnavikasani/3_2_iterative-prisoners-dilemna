e0c0db0e8056aea9d9f7aa921441f6c94102f334
GarrettIsenbarger <10752@sacs.k12.in.us>
2016-12-20 10:04:56 -0500

    garrett
    change

--- prisoners dilemma.py
+++ prisoners dilemma.py
@@ -1,4 +1,4 @@
-@@ -0,0 +1,384 @@
+
 from __future__ import print_function
 
 ##### 
@@ -47,7 +47,7 @@ collude = example0
 modules = [example0, example1, example2, example3, example4, example5, example6, example7,
 team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10, 
 team11, team12, team13, team14]
-
+#change garrett
 for module in modules:
     reload(module)
     print ('reloaded',module)
